230c9de230e89bcbdb66b2790e27db26a26a92d9
fengzhang90 <inter18099@hotmail.com>
2024-03-12 13:08:59 +0800

    init: homepage with 3 components

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import HomepageNav from './components/HomepageNav.vue'
+import HomepageFigure from './components/HomepageFigure.vue'
+</script>
+
+<template>
+  <HomepageNav />
+  <HomepageFigure imgUrl="https://cdn-image2.fengrin.me/xiandehuang%2Fhammer-1868956_1280.jpg" text1="Hammer S1" text2="得锤，得尝所锤，OK"/>
+  <HomepageFigure imgUrl="https://cdn-image2.fengrin.me/xiandehuang%2Fmarguerite-729510_1280.jpg" text3="Xiandehuang 火星" text4="2099 年"/>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/HomepageNav.vue
@@ -0,0 +1,44 @@
+<script>
+
+</script>
+<template>
+<div class="outterBar">
+    <div class="innerBar">
+        <div class="col"><img class="col" src="https://cdn-image2.fengrin.me/xiandehuang%2F2018new_doge02_org.png" width="14px"></div>
+        <div class="col">商店</div>
+        <div class="col">锤</div>
+        <div class="col">槌</div>
+        <div class="col">改锥</div>
+        <div class="col">螺丝</div>
+        <div class="col">螺母</div>
+        <div class="col">棒槌</div>
+        <div class="col">工具箱</div>
+        <div class="col">扳子</div>
+        <div class="col">剪刀</div>
+        <div class="col"><img src="https://cdn-image2.fengrin.me/xiandehuang%2Ffangdajing.png" width="14px"></div>
+        <div class="col"><img src="https://cdn-image2.fengrin.me/xiandehuang%2Fbag.png" width="14px"></div>
+    </div>
+</div>
+</template>
+<style>
+    .innerBar {
+      display: grid;
+      grid-template-columns: repeat(13, 1fr);
+      grid-gap: 10px;
+      background-color: rgb(20, 19, 19);
+      width: 75%;
+    }
+
+    .col {
+      padding: 10px;
+      display: flex;
+      align-items: center;
+    }
+    .outterBar {
+        display: grid;
+        place-items: center; /* 水平和垂直居中 */
+        background-color: rgb(20, 19, 19);
+        color: gray;
+        font-size: 9pt;
+    }
+</style>
