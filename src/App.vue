cefd63e1998e2c45892b539afd45702bbf8e86ad
fengzhang90 <inter18099@hotmail.com>
2024-03-12 13:59:51 +0800

    fix: text typo

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@ import HomepageFigure from './components/HomepageFigure.vue'
 <template>
   <HomepageNav />
   <HomepageFigure imgUrl="https://cdn-image2.fengrin.me/xiandehuang%2Fhammer-1868956_1280.jpg" text1="Hammer S1" text2="得锤，得尝所锤，OK"/>
-  <HomepageFigure imgUrl="https://cdn-image2.fengrin.me/xiandehuang%2Fmarguerite-729510_1280.jpg" text3="Xiandehuang 火星" text4="2099 年"/>
+  <HomepageFigure imgUrl="https://cdn-image2.fengrin.me/xiandehuang%2Fmarguerite-729510_1280.jpg" text3="Pear 火星" text4="2099 年"/>
 
 </template>
 
