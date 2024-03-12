1d578580ab3932712887ad5585069c0627caf3f9
fengzhang90 <inter18099@hotmail.com>
2024-03-12 17:47:50 +0800

    fix: add contact me

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@ import HomepageFigure from './components/HomepageFigure.vue'
   <HomepageNav />
   <HomepageFigure imgUrl="https://cdn-image2.fengrin.me/xiandehuang%2Fhammer-S1.png" text1="Hammer S1" text2="得锤，得尝所锤，OK"/>
   <HomepageFigure imgUrl="https://cdn-image2.fengrin.me/xiandehuang%2Fmarguerite-729510_1280.jpg" text3="Tool 火星" text4="2099 年"/>
-
+  <a href="mailto:inter18099#hotmail.com">Contact me</a>
 </template>
 
 <style scoped>
