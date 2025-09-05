a07d302f3b3b4dc811c02dc8022e4baf5ebce31b
fengz <ruifengz@outlook.com>
2025-09-06 00:03:58 +0800

    small fixes: change bg-color and image bg

--- src/App.vue
+++ src/App.vue
@@ -10,18 +10,18 @@ import HomepageNewStore from './components/HomepageNewStore.vue';
     <HomepageFigure imgUrl="https://cdn-image2.ruifeng.me/xiandehuang%2Fhammer-S1.png" text1="Hammer pro" text2="得锤，敲敲打打，修葺一新"/>
   </div>
   <div class="homepageNewStore">
-    <HomepageNewStore imgUrl="https://cdn-image2.ruifeng.me/xiandehuang/xiandehuang_marguerite-729510_1280.png" text1="Tool 火星" text2="2099 年"/>
+    <HomepageNewStore imgUrl="https://cdn-image2.ruifeng.me/xiandehuang/xiandehuang_marguerite2.png" text1="Tool 火星" text2="2099 年"/>
   </div>
   <a href="mailto:inter18099#hotmail.com">Contact me</a>
 </template>
 <style>
 .homepageHero {
-  background-color: gray;
+  background-color: #e0e0e0;
   width:100%;
   margin-bottom: 10px;
 }
 .homepageNewStore {
-  background-color: gray;
+  background-color: #e0e0e0;
   width:100%;
   margin-bottom: 10px;
 }
